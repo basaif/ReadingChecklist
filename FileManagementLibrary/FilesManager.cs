085af42da105f62d34cac3b44fa0507f8f825f48
basaif <iman.basaif@gmail.com>
2022-06-04 13:38:16 +0300

    Organize FilesManager Class

--- FileManagementLibrary/FilesManager.cs
+++ FileManagementLibrary/FilesManager.cs
@@ -2,56 +2,16 @@
 {
     public class FilesManager
     {
-        public FilesManager(string location)
-        {
-            _location = location;
-        }
-
         private string _location;
-
         public void ChangeLocation(string newLocation)
         {
             _location = newLocation;
         }
 
-        public bool DoesLocationExist(string location)
+        public FilesManager(string location)
         {
-            return Directory.Exists(location);
+            _location = location;
         }
-
-        public List<string> GetAllFilePathsInLocation()
-        {
-            string[] files = Directory.GetFiles(_location, "*", SearchOption.AllDirectories);
-
-            return files.ToList();
-        }
-
-        public string GetFileNameFromFilePath(string filePath)
-        {
-            return Path.GetFileName(filePath);
-        }
-
-        public List<string> GetFoldersFromFilePath(string filePath)
-        {
-            List<string> folders = new();
-
-            string fileName = GetFileNameFromFilePath(filePath);
-
-            List<string> tokens = GetListPathParts(filePath);
-
-            foreach (string t in tokens)
-            {
-                if (!(_location.Contains(t) || t == fileName))
-                {
-                    if (!folders.Contains(t))
-                    {
-                        folders.Add(t);
-                    }
-                }
-            }
-            return folders;
-        }
-
         public List<(List<string> Folders, string FileName)> GetAllFoldersFileNamePairsInLocation()
         {
             List<(List<string> Folders, string FileName)> foldersNamePairs = new();
@@ -65,16 +25,48 @@
 
             return foldersNamePairs;
         }
+        private List<string> GetAllFilePathsInLocation()
+        {
+            string[] files = Directory.GetFiles(_location, "*", SearchOption.AllDirectories);
 
+            return files.ToList();
+        }
         public (List<string> Folders, string FileName) GetFoldersFileNamePairs(string filePath)
         {
             return (GetFoldersFromFilePath(filePath), GetFileNameFromFilePath(filePath));
         }
+        public List<string> GetFoldersFromFilePath(string filePath)
+        {
+            List<string> folders = new();
 
-        public List<string> GetListPathParts(string filePath)
+            string fileName = GetFileNameFromFilePath(filePath);
+
+            List<string> tokens = GetListPathParts(filePath);
+
+            AddTokensToFolders(tokens, folders, fileName);
+
+            return folders;
+        }
+        private string GetFileNameFromFilePath(string filePath)
+        {
+            return Path.GetFileName(filePath);
+        }
+        private List<string> GetListPathParts(string filePath)
         {
             return filePath.Split('\\').ToList();
         }
-
+        private void AddTokensToFolders(List<string> tokens, List<string> folders, string fileName)
+        {
+            foreach (string t in tokens)
+            {
+                if (!(_location.Contains(t) || t == fileName))
+                {
+                    if (!folders.Contains(t))
+                    {
+                        folders.Add(t);
+                    }
+                }
+            }
+        }
     }
 }
